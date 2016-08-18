adac63d5cc0db3be971903c4239ff1a09d255bfe
ssoso27 <sohi8290@naver.com>
2016-08-18 21:24:15 +0900

    구조체 및 변수 생성

--- YSH/Target/Target/Target.cpp
+++ YSH/Target/Target/Target.cpp
@@ -1,11 +1,43 @@
 #include "Header.h"
 #include "Target.h"
 
+
+// 전역변수
+
+// 구조체 변수
+STAGE Stage;
+TARGET_O Target_O[10];
+TARGET_X Target_X[10];
+TARGET_HEART Target_heart[3];
+GAMESTATUS GameStatus;
+
+// 출력 관련
+int OArray[3] = {1,2,3};// O 개수 배열
+int XArray[3] = {1,2,3};// X 개수 배열
+						// ♥은 모든 Stage에서 3개
+
+// 함수
+
+
+
+void CreateTarget() // Target 생성 함수
+{
+	// O 생성
+
+	// X 생성
+
+	// ♥ 생성
+}
+
 // 프레임워크 함수
 
 void Init()
 {
+	// Target들 초기화
+	CreateTarget();
 
+	// 게임 상태 초기화
+	GameStatus = START;
 }
 
 void Update()
--- YSH/Target/Target/Target.h
+++ YSH/Target/Target/Target.h
@@ -1,6 +1,40 @@
 #pragma once
 
+// 구조체
+typedef struct _STAGE
+{
+	int level; // Stage level
+	// int 배열로 초기화 
+	int targetCount; // 총 target의 개수
+	int OCount; // O target의 개수
+	int XCount; // X target의 개수
+	int heartCount; // ♥ target의 개수
+} STAGE;
+
+typedef struct _TARGET_O
+{
+	int X;
+	int Y;
+} TARGET_O;
+
+typedef struct _TARGET_X
+{
+	int X;
+	int Y;
+} TARGET_X;
+
+typedef struct _TARGET_HEART
+{
+	int X;
+	int Y;
+} TARGET_HEART;
+
+// 열거형
+typedef enum _GAMESTATUS {START, INIT, READY, RUNNING, STOP, SUCCESS, FAILED, RESULT} GAMESTATUS;
+
+
 // 함수원형
+void CreateTarget();
 
 void Init();
 void Update();
