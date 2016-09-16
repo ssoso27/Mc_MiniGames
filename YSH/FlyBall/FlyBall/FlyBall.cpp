e65ba3d2b790cba5a0aa03e55df8357d9992fe01
ssoso27 <sohi8290@naver.com>
2016-09-16 12:09:58 +0900

    FlyBall 생성 및 공 움직임 구현

---
+++ YSH/FlyBall/FlyBall/FlyBall.cpp
@@ -0,0 +1,205 @@
+#include "Header.h"
+#include "Screen.h"
+#include "FlyBall.h"
+
+// 구조체 변수 & 열거형
+enum ControlKeys
+{
+	UP = 72,
+	DOWN = 80,
+	LEFT = 75,
+	RIGHT = 77,
+	SPACE = 32,
+	ESC = 27
+};
+
+PLAYER Player;
+
+
+// 함수
+// Player의 이동
+void PlayerMove(clock_t CurTime)
+{
+	char temp[20];
+	if (Player.IsReady == 0) // 준비상태가 아니면
+	{
+		if (CurTime - Player.OldTime > Player.MoveTime) // 이동제한시간 경과
+		{
+			Player.OldTime = CurTime;
+
+			// 방향에 따른 좌표 변화 
+			switch (Player.Direction)
+			{
+			case TOP:
+				sprintf(temp, "TOP");
+				ScreenPrint(10, 2, temp);
+				//if (Collision(Player.X, Player.Y - 1) == 0) // 이동할 좌표에서 충돌이 안 일어나면 
+				{
+					Player.Y--; // 이동한다
+				}
+				// 충돌이 일어났을 경우, Collision에서 Player.Direction이 변경된다.
+				break;
+
+			case TOP_RIGHT:
+				//if (Collision(Player.X + 1, Player.Y - 1) == 0)
+				{
+					Player.X++;
+					Player.Y--;
+				}
+				break;
+
+			case BOT_RIGHT:
+				//if (Collision(Player.X + 1, Player.Y + 1) == 0)
+				{
+					Player.X++;
+					Player.Y++;
+				}
+				break;
+
+			case BOTTOM:
+				//if (Collision(Player.X, Player.Y + 1) == 0)
+				{
+					Player.Y++;
+				}
+				break;
+
+			case BOT_LEFT:
+				//if (Collision(Player.X - 1, Player.Y + 1) == 0)
+				{
+					Player.X--;
+					Player.Y++;
+				}
+				break;
+
+			case TOP_LEFT:
+				//if (Collision(Player.X - 1, Player.Y - 1) == 0)
+				{
+					Player.X--;
+					Player.Y--;
+				}
+				break;
+
+			}
+		}
+		if (Player.X < 0 || Player.X >BOARD_WIDTH || Player.Y < 0 || Player.Y > BOARD_HEIGH) // 벽에 충돌
+		{
+			Player.IsReady = 1;
+			Player.Direction = TOP;
+		//	Player.X = Bar.X[1];
+		//	Player.Y = Bar.Y - 1;
+		}
+	}
+	else // 준비 상태면
+	{
+	//	Player.X = Bar.X[1];
+	//	Player.Y = Bar.Y - 1;
+	}
+}
+
+void KeyControl(int key)
+{
+	int direction;
+
+	switch (key)
+	{
+	case SPACE:
+		//(Player.IsReady == 1) ? (Player.IsReady = 0) : (Player.IsReady = 1); // Player.IsReady 바꿈 
+		if (Player.IsReady == 1)
+		{
+			Player.OldTime = clock();
+			Player.IsReady = 0; // 준비 O -> 준비 X
+		}
+		break;
+
+	case 'S': case 's': // ↑
+		direction = 0;
+		Player.Direction = (DIRECT)direction;
+		Player.OldTime = clock();
+		break;
+
+	case 'D': case 'd': // ↗
+		direction = 1;
+		Player.Direction = (DIRECT)direction;
+		Player.OldTime = clock();
+		break;
+
+	case 'C': case 'c': // ↘
+		direction = 2;
+		Player.Direction = (DIRECT)direction;
+		Player.OldTime = clock();
+		break;
+
+	case 'X': case 'x': // ↓
+		direction = 3;
+		Player.Direction = (DIRECT)direction;
+		Player.OldTime = clock();
+		break;
+
+	case 'Z': case 'z': // ↙
+		direction = 4;
+		Player.Direction = (DIRECT)direction;
+		Player.OldTime = clock();
+		break;
+
+	case 'A': case 'a': // ↖
+		direction = 5;
+		Player.Direction = (DIRECT)direction;
+		Player.OldTime = clock();
+		break;
+
+	default:
+		break;
+	}
+}
+
+// 프레임워크 함수
+void Init()
+{
+	Player.X = 2;
+	Player.Y = 2;
+	Player.Direction = TOP;
+	Player.OldTime = clock();
+	Player.IsReady = 1;
+	Player.MoveTime = 130;
+}
+
+void Update()
+{
+	clock_t CurTime = clock(); // 현재 시각
+
+	PlayerMove(CurTime);
+}
+
+void Render()
+{
+	ScreenClear();
+
+	ScreenPrint(Player.X, Player.Y, "●");
+
+	ScreenFlipping();
+}
+
+void main()
+{
+	int key;
+
+	Init();
+	ScreenInit();
+	
+	while (1)
+	{
+		if (_kbhit())
+		{
+			key = _getch();
+
+			if (key == 'q' || key == 'Q')
+				break;
+
+			KeyControl(key);
+
+		}
+
+		Update();
+		Render();
+	}
+}
---
+++ YSH/FlyBall/FlyBall/FlyBall.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#define BOARD_WIDTH 78
+#define BOARD_HEIGH 24
+
+// 열거형
+typedef enum _DIRECT { TOP, TOP_RIGHT, BOT_RIGHT, BOTTOM, BOT_LEFT, TOP_LEFT } DIRECT; // 방향 설정. 시계방향. 
+
+// 구조체
+typedef struct _BLOCK
+{
+	int X;
+	int Y;
+} BLOCK;
+
+typedef struct _GOAL
+{
+	int X;
+	int Y;
+} GOAL;
+
+typedef struct _PLAYER
+{
+	int X, Y;
+	int IsReady; // 준비? 이동?
+	int Life;
+	DIRECT Direction; // 공 이동방향
+	clock_t MoveTime;
+	clock_t OldTime;
+} PLAYER;
+
+// 함수원형
+void PlayerMove(clock_t );
+void KeyControl(int);
+
+void Init();
+void Update();
+void Render();
+void FlyBall();
