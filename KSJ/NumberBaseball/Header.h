58d47e0095b3fe1c2cb2047a08aa35a8a8942959
KSJ <mr_magnet@naver.com>
2016-10-24 23:09:32 +0900

    파일재업과 인트로

---
+++ KSJ/NumberBaseball/Header.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+#include <windows.h>
+#include <time.h>
+#include "Screen.h"
+/*
+#define ESC 27
+#define SPACE 32
+#define LEFT 75
+#define RIGHT 77
+#define UP 72
+#define DOWN 80
+*/
+#pragma warning(disable:4996)
